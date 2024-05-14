10191ef3ab2022d866d19b400bc2f7d468b84533
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-14 23:28:58 +1200

    refactoring

--- whistleblower/admin.py
+++ whistleblower/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from .models import CustomUser, Report, ReportFile
-from django.contrib.auth.admin import UserAdmin
-# Register your models here.
 
 admin.site.register(Report)
 admin.site.register(ReportFile)
 
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'is_site_admin']
-    list_filter = [ 'is_site_admin']
+    list_filter = ['is_site_admin']
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
@@ -19,7 +18,8 @@ class CustomUserAdmin(admin.ModelAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_active', 'is_site_admin')}
-        ),
+            'fields': (
+                'username', 'email', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_active',
+                'is_site_admin')}
+         ),
     )
-
